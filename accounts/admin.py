09ccad8a96c814ae6f781791cdb91ef723374657
Deias <saied_am@hotmail.com>
2023-02-01 18:44:06 +0100

    feat: adding User to admin panel and avoir modifying the password

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """Custom user admin.
+
+    Customize the admin panel for the custom user model.
+    Make password field not editable.
+    """
+
+    list_display = (
+        "email", "first_name", "last_name", "username", "role", "is_active"
+    )
+    ordering = ("-date_joined",)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, CustomUserAdmin)
