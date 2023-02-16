7b84aa9ca96bf3e9fe3fb0e93312549201ac5c5b
Deias <saied_am@hotmail.com>
2023-02-16 21:04:15 +0100

    adding message when registration is succes and showung error messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+
 from accounts.forms import UserForm
+
+User = get_user_model()
 
 
 def register_user(request):
@@ -7,8 +12,13 @@ def register_user(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+            user = form.save(commit=False)
+            password = request.POST.get('password')
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, 'Your account have been created successfully')
+            return redirect('register_user')
     else:
         form = UserForm()
 
