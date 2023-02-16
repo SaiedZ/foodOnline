3fb596fff2f5068055a98625fbb9f21014cc8514
Deias <saied_am@hotmail.com>
2023-02-16 19:33:58 +0100

    adding accounts urls

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,9 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    # accounts
+    path('accounts', include('accounts.urls')),
 ]
 # to serve media file in development
 if settings.DEBUG:
