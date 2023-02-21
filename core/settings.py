b073d6ee3222bd9690a9af2fdb5da09daea83c27
Deias <saied_am@hotmail.com>
2023-02-21 17:38:56 +0100

    updating styles and removing dropdown from navbar

--- core/settings.py
+++ core/settings.py
@@ -143,3 +143,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
